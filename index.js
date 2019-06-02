920cbea1a585b8f3428397d2451174fa2bdee83f
Monica Lee <mylee.715@gmail.com>
2019-06-02 02:46:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,6 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key)
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
