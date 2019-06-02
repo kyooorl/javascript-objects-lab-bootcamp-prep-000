c336e0d8f7af62ef08211bb958e6ce59780786f5
Monica Lee <mylee.715@gmail.com>
2019-06-02 02:32:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = object.assign({}, obj);
+  var newObj = Object.assign({}, obj);
   delete newObj.key;
   return newObj;
 }
